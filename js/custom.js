119cde7438438d28c3420e25956e04ab09448114
tndbs0754 <169110561+tndbs0754@users.noreply.github.com>
2024-06-12 22:15:56 +0900

    디저트39

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function () {
+
+
+
+    $('main_visual_slide').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+})
