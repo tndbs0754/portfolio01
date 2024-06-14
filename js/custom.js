988d41f78aa4a22bee95e4ef5ff331e6383fe0db
tndbs0754 <169110561+tndbs0754@users.noreply.github.com>
2024-06-14 15:39:40 +0900

    디저트39 홈페이지 만들기

--- js/custom.js
+++ js/custom.js
@@ -2,12 +2,19 @@ $(function () {
 
 
 
-    $('main_visual_slide').slick({
+    $('.main_product_slide').slick({
+        slidesToShow: 4,
         arrows: false,
         dots: true,
-        autoplay: true,
-        pauseOnHover: false,
-        pauseOnFocus: false,
     });
+
+    $('.main_product02 .arrows .prev').on('click', function () {
+        $('.main_product_slide').slick('slickPrev')
+    })
+    $('.main_product02 .arrows .next').on('click', function () {
+        $('.main_product_slide').slick('slickNext')
+    })
+
+
 
 })
